8188c0f2592ded59fdf3e630742b13f9a8ecc3d3
ZAF07 <zaffere07@gmail.com>
2023-07-11 02:22:12 +0800

    added description to the solution for best time to buy/sell stocks sliding window technique

--- arrays/best-time-to-buy-sell-stocks/best_time_to_buy_sell_stocks_sliding_window_test.go
+++ arrays/best-time-to-buy-sell-stocks/best_time_to_buy_sell_stocks_sliding_window_test.go
@@ -30,16 +30,21 @@ func TestBestProfitSlidingWindow(t *testing.T) {
 
 // Sliding window
 func bestProfit(stocks []int) int {
+	// Start off at first two elements of the array
 	buy, sell := 0, 1
 	profit := 0
+	// As long as the sell position (right index) does not exceed the length of the stocks (array) we keep calculating profit/loss
 	for sell < len(stocks) {
+		// If buy (left index value) is smaller than the sell value (right index value), that means we make a profit. Calculate the current profit and compare the previous profit
 		if stocks[buy] < stocks[sell] {
 			sum := stocks[sell] - stocks[buy]
 			profit = int(math.Max(float64(profit), float64(sum)))
 			fmt.Println(sum, profit, buy, sell)
 		} else {
+			// If buy is larger than the next sell (sell is smaller than buy), we set the buy value to the sell value
 			buy = sell
 		}
+		// And finally we increase the sell index by 1 for the next comparison
 		sell += 1
 	}
 	return int(profit)

0f8fc2d5266cc3b91c9d63cf7b14fd0d171a62b0
ZAF07 <zaffere07@gmail.com>
2023-07-18 20:20:27 +0800

    added one more solution for remove duplicates

--- arrays/remove-duplicates/remove_duplicates_test.go
+++ arrays/remove-duplicates/remove_duplicates_test.go
@@ -32,44 +32,58 @@ func TestRemoveDuplicates(t *testing.T) {
 	for _, tt := range tests {
 		testName := fmt.Sprintf("Ecpected : %v", tt.want)
 		t.Run(testName, func(t *testing.T) {
-			got := RemoveDuplicates(tt.nums)
+			// got := RemoveDuplicates(tt.nums)
+			got := RemoveDuplicates2(tt.nums)
 			assert.Equal(t, tt.want, got)
 		})
 	}
 }
 
+/* Implementation #1
+Here we have a variable that stores the index of the array starting at position 0
+At every iteration, if we have not seen the value, we add it to the map and adjust the array at current position to the current element
+We only increase the position if we have not seen the current value and after we have added the current value to the array
+
+*/
 func RemoveDuplicates(arr []int) int {
-	fmt.Println("Given slice: ", arr)
 	seen := make(map[int]int)
 	idx := 0
 	for _, val := range arr {
-		fmt.Println("############START#############")
 
-		fmt.Println("Current idx is at: ", idx)
-
-		fmt.Printf("Checking if %d has been seen before\n", val)
 		// If the current value is NOT seen/visited in the loop yet
 		if _, ok := seen[val]; !ok {
-			fmt.Printf("Value: %d has NOT been seen\n", val)
 
-			fmt.Printf("Adding value: %d to the seen map\n", val)
 			// add value to the map for future reference
 			seen[val] = val
 
-			fmt.Printf("Setting slice element at Index:%d to Value:%d\n", idx, val)
 			// set the slice at current index to the value
 			arr[idx] = val
-			fmt.Println("This is the slice after setting: ", arr)
 
-			fmt.Printf("Incrementing idx from %d to %d\n", idx, idx+1)
 			// Increment idx only when the value has not been seen yet(in other words, idx remains the same if the value has been seen before) so that we can replace that value with a value that has NOT been seen. With this we know that any values after arr[idx] has been seen before so we can trim the slice(arr[:idx]) up to the idx and return the length as the result
 			idx++
-		} else {
-			fmt.Printf("Value:%d HAS been seen. Skipping\n", val)
 		}
-		fmt.Println("############END#############")
 	}
 	arr = arr[:idx]
-	fmt.Println("Result: ", arr)
 	return len(arr)
+}
+
+/*  Implementation #2
+This method loops the given array once.
+At each iteration, the array becomes smaller if ther are any duplicates
+*/
+func RemoveDuplicates2(nums []int) int {
+	seenMap := make(map[int]int)
+
+	// Loop the array starting from the end
+	for i := len(nums) - 1; i >= 0; i-- {
+		// If we've seen the value before, adjust the array removing the current seen element
+		if position, seen := seenMap[nums[i]]; seen {
+			nums = append(nums[:position], nums[position+1:]...)
+		}
+
+		// Regardless of whether we've seen the current element or not, we update its latest seen position
+		seenMap[nums[i]] = i
+
+	}
+	return len(nums)
 }
--- main.go
+++ main.go
@@ -5,18 +5,26 @@ import "fmt"
 // Package main is for quick testing of written code without the Go Test package
 
 func main() {
-	nums := []int{1, 5, 2, 4, 3}
-	target := 9
-	res := TwoSum(nums, target)
-	fmt.Println(res)
+	// nums := []int{1, 5, 2, 4, 3}
+	// target := 9
+	// res := TwoSum(nums, target)
+	// fmt.Println(res)
 
-	bub := Bubble(nums)
-	fmt.Println(bub)
+	// bub := Bubble(nums)
+	// fmt.Println(bub)
 
-	bNums := []int{1, 2, 4, 5, 7, 8}
-	bTarget := 2
-	bin := BinaryS(bNums, bTarget)
-	fmt.Println(bin)
+	// bNums := []int{1, 2, 4, 5, 7, 8}
+	// bTarget := 2
+	// bin := BinaryS(bNums, bTarget)
+	// fmt.Println(bin)
+
+	// nums := []int{4, 5, 6, 0, 1, 2}
+	// res := rBinary(nums, 0)
+	// fmt.Println(res)
+
+	rNums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4}
+	resr := removeDups(rNums)
+	fmt.Println("REMOVE DUPS: ", resr)
 }
 
 func TwoSum(nums []int, target int) []int {
@@ -63,4 +71,95 @@ func BinaryS(nums []int, target int) int {
 		}
 	}
 	return -1
+}
+
+func rBinary(nums []int, target int) int {
+	pivot := findPivot(nums)
+
+	if target > nums[0] {
+		return rbinary(nums, 0, pivot-1, target)
+	} else {
+		return rbinary(nums, pivot, len(nums)-1, target)
+	}
+}
+
+func rbinary(nums []int, left, right, target int) int {
+	for left <= right {
+		mid := (left + right) / 2
+		fmt.Println(nums[mid], target)
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] > target {
+			right = mid - 1
+		}
+
+		if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	return -100
+}
+
+//        lr
+// [4,5,6,0,1,2]
+func findPivot(nums []int) int {
+	left, right := 0, len(nums)-1
+	ref := nums[0]
+	for left < right {
+		mid := (left + right) / 2
+
+		if nums[mid] >= ref {
+			left = mid + 1
+		}
+
+		if nums[mid] < ref {
+			right = mid
+		}
+	}
+	return left
+}
+
+// map 4:10 3: 9>8>7 2:6 1:4>3 0:1
+// index : 0
+//                  v
+// [0,0,1,1,1,2,2,3,3,3,4] 10
+//                v
+// [0 0 1 1 1 2 2 3 3 4] 9
+//            v
+// [0 0 1 1 1 2 2 3 4]
+//          v
+// [0 0 1 1 1 2 3 4]
+func removeDups(nums []int) []int {
+	// nums = append(nums[:9], nums[10:]...)
+	// fmt.Println(nums)
+
+	// nums = append(nums[:8], nums[9:]...)
+	// fmt.Println(nums)
+
+	// nums = append(nums[:6], nums[7:]...)
+	// fmt.Println(nums)
+
+	// nums = append(nums[:4], nums[5:]...)
+	// fmt.Println(nums)
+
+	// nums = append(nums[:3], nums[4:]...)
+	// fmt.Println(nums)
+
+	// nums = append(nums[:1], nums[2:]...)
+	// fmt.Println(nums)
+	seenMap := make(map[int]int)
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if position, seen := seenMap[nums[i]]; seen {
+			nums = append(nums[:position], nums[position+1:]...)
+			// seenMap[nums[i]] = i
+		}
+
+		seenMap[nums[i]] = i
+
+	}
+	return nums
 }

344909ba3a311ac81b04f1d83a0ee56767d1bb9b
ZAF07 <zaffere07@gmail.com>
2023-07-31 12:36:27 +0800

    added problem description for longest palindrome problem

--- palindrome/longest_palindrome_test.go
+++ palindrome/longest_palindrome_test.go
@@ -7,6 +7,24 @@ import (
 	"gopkg.in/go-playground/assert.v1"
 )
 
+/*
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+*/
+
 func TestLongestPalindrome(t *testing.T) {
 	tests := []struct {
 		s    string

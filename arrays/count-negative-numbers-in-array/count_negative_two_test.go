4791e1d6f8ea576514e2a2666078594f1b3164ee
ZAF07 <zaffere07@gmail.com>
2023-10-13 19:17:27 +0800

    added description for solution

--- arrays/count-negative-numbers-in-array/count_negative_two_test.go
+++ arrays/count-negative-numbers-in-array/count_negative_two_test.go
@@ -25,22 +25,28 @@ func TestCountNegativeTwo(t *testing.T) {
 	}
 }
 
+/*
+The runtime for this algo is O(n * m) best case only when n > m.
+When m > n, this algo is worst than O(n^2)
+*/
 func CountNegativeIntegersTwo(grid [][]int) int {
 	res := 0
 	for i := 0; i <= len(grid)-1; i++ {
 		curArray := grid[i]
 		curNum := curArray[len(curArray)-1]
 
+		// As long as the current integer is a negative number, we continuw with the loop
 		for curNum < 0 {
-			fmt.Println("a: ", curArray)
+			// Imcrement the result at each loop. (This only happens when current number is a negative number)
+			res++
 			if len(curArray) > 1 {
+				// Pop the last element from the current array
 				curArray = curArray[:len(curArray)-1]
+				// Update the current number
 				curNum = curArray[len(curArray)-1]
 			} else {
-				res++
 				break
 			}
-			res++
 		}
 	}
 	return res

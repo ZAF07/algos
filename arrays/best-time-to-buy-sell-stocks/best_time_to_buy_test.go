bd28488c05b4589b152974ef28daa145ecdf45f2
ZAF07 <zaffere07@gmail.com>
2023-11-18 21:33:42 +0800

    edited problem description

--- arrays/best-time-to-buy-sell-stocks/best_time_to_buy_test.go
+++ arrays/best-time-to-buy-sell-stocks/best_time_to_buy_test.go
@@ -8,8 +8,24 @@ import (
 )
 
 /*
-	Find the best time to buy & sell stocks to get the max profit
-	Example: [2,1,4,5,3] == 4 (we buy at 1 & sell at 5 to make a $4 profit)
+121. Best Time to Buy and Sell Stock
+Easy
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
 
 */
 

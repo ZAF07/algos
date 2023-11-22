ff01e3def531cd6b8496947a77540fec769366c4
ZAF07 <zaffere07@gmail.com>
2023-11-22 22:49:49 +0800

    added description for balloon problem

--- balloon/balloon_test.go
+++ balloon/balloon_test.go
@@ -35,6 +35,10 @@ func TestBalloon(t *testing.T) {
 	}
 }
 
+// Store count of each characters in a map (BALLOON).
+// At the end we simply return the minimum number of occurances between these characters (BALLOON)
+// Because if there is enough characters to form the word ballon, all values in count will be the same.
+
 func balloon(str string) int {
 	if len(str) < 7 {
 		return 0

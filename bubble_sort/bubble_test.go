89c7c3fa62c2b69b3c152ff785050e35bd24c975
ZAF07 <zaffere07@gmail.com>
2024-02-24 15:30:33 +0800

    added bubble sort with a catch solution

--- bubble_sort/bubble_test.go
+++ bubble_sort/bubble_test.go
@@ -24,17 +24,20 @@ func TestBubbleSort(t *testing.T) { // Run 'go test <path> -test.v' to see logs 
 		nums []int
 		want []int
 	}{
-		{[]int{5, 2, 9, 6, 1, 8}, []int{1, 2, 5, 6, 8, 9}},
-		{[]int{5, 2, 3, 6, 1, 4}, []int{1, 2, 3, 4, 5, 6}},
-		{[]int{15, 12, 13, 16, 11, 14}, []int{11, 12, 13, 14, 15, 16}},
-		{[]int{15, 2, 3, 26, 1, 45}, []int{1, 2, 3, 15, 26, 45}},
-		{[]int{50, 23, 31, 62, 12, 4}, []int{4, 12, 23, 31, 50, 62}},
+		{[]int{0, 1, 4, 3, 0, 2}, []int{1, 2, 3, 4, 0, 0}},
+		// {[]int{5, 2, 9, 6, 1, 8}, []int{1, 2, 5, 6, 8, 9}},
+		// {[]int{5, 2, 3, 6, 1, 4}, []int{1, 2, 3, 4, 5, 6}},
+		// {[]int{15, 12, 13, 16, 11, 14}, []int{11, 12, 13, 14, 15, 16}},
+		// {[]int{15, 2, 3, 26, 1, 45}, []int{1, 2, 3, 15, 26, 45}},
+		// {[]int{50, 23, 31, 62, 12, 4}, []int{4, 12, 23, 31, 50, 62}},
 	}
 
 	for _, tt := range tests {
 		testName := fmt.Sprintf("Given : %v, Want: %v", tt.nums, tt.want)
 		t.Run(testName, func(t *testing.T) {
-			got := Bubble(tt.nums)
+			// got := Bubble(tt.nums)
+			// got := bubbleTwo(tt.nums)
+			got := bubbleZeroLargest(tt.nums)
 			// t.Logf("RETURNED: %v", got)
 			assert.Equal(t, tt.want, got)
 		})
@@ -64,6 +67,22 @@ func bubbleTwo(nums []int) []int {
 	for i := 0; i < len(nums); i++ { // First pass goes through the entire array.
 		for j := 0; j > len(nums)-1-i; j++ { // inner loop keeps decreasing because we know that at each pass, the last element will be the largest element
 			if nums[j] > nums[j+1] { // if the current num is > next num, we swap positions
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+	return nums
+}
+
+// Bubble sort with a catch. 0 is considered the largest number. All the same as regular Bubble Sort, just that we need some extra conditional to check that if value is 0, it is considered > right
+func bubbleZeroLargest(nums []int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums)-1-i; j++ {
+			if nums[j] > nums[j+1] || (nums[j] == 0 && nums[j+1] > 0) {
+				// Check if integer on the left is > 0 and if integer directly to the right is 0. if so, continue (because 0 is largest)
+				if nums[j] > 0 && nums[j+1] <= 0 {
+					continue
+				}
 				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}

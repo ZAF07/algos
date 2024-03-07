2d949c035d836586c57071b2da69df0457348249
ZAF07 <zaffere07@gmail.com>
2024-03-07 12:12:09 +0800

    added a cleaner solution for first & last position in array problem. easier to read.

--- binary-search/first_and_last_position_test.go
+++ binary-search/first_and_last_position_test.go
@@ -49,13 +49,22 @@ func TestFirstAndLast(t *testing.T) {
 		testName := fmt.Sprintf("Given: %v || want: %v", tt.given, tt.want)
 
 		t.Run(testName, func(t *testing.T) {
+			// Initial implementation
 			got := fnl2(tt.given, tt.target)
 			assert.Equal(t, tt.want, got)
+
+			// Cleaner code version
+			got3 := Fnl3(tt.given, tt.target)
+			assert.Equal(t, tt.want, got3)
+
+			// Abstracted version
+			got2 := fnl2(tt.given, tt.target)
+			assert.Equal(t, got2, tt.want)
 		})
 	}
 }
 
-// O(n) version
+// O(log n) time, O(1) space
 func FnL(nums []int, target int) []int {
 	res := make([]int, 2)
 
@@ -99,7 +108,7 @@ func FnL(nums []int, target int) []int {
 	return res
 }
 
-// O(logn) version
+// O(logn) time, O(1) space
 func fnl2(nums []int, target int) []int {
 	start := findTarget(nums, target, true)
 	if start == -1 {
@@ -144,4 +153,46 @@ func findTarget(nums []int, target int, findMin bool) int {
 
 	}
 	return idx
+}
+
+// Cleaner code version. Still O(logn) time and O(1 space)
+func Fnl3(nums []int, target int) []int {
+	// result slice
+	res := []int{-1, -1}
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		// Find the mid point of the slice
+		mid := (l + r) / 2
+
+		// The current value at mid point of slice
+		cur := nums[mid]
+
+		if cur == target {
+			tmp := mid
+			// find the start position. Keep moving left until we either get a value that != target or have reached the start of the array
+			for tmp > 0 && nums[tmp-1] == target {
+				tmp--
+			}
+			res[0] = tmp
+			tmp = mid
+
+			// Now find the end position. Keep going as long as tmp <= len(nums) -2 (-2 because we dont want to go out of bounds in the +1 step) and next element ==target
+			for tmp <= len(nums)-2 && nums[tmp+1] == target {
+				tmp++
+			}
+			res[1] = tmp
+			return res
+		}
+
+		if cur > target {
+			r = mid - 1
+		}
+
+		if cur < target {
+			l = mid + 1
+		}
+
+	}
+	return res
 }
